5405de50520e4fe6b1069ff39472cc9bfd320914
Anooja Jayaraj <anoojajayaraj@gmail.com>
2024-03-22 11:55:25 +0100

    more tests

---
+++ Periodic_potential/tests/graphene_test/non_standard_cell_pbc/length_10nm/potential_single.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+value=0
+exponent=-1
+potential=$(bc <<<"scale=1; $value*10^$exponent")
+echo $potential
+dirname=potential_$potential
+mkdir -p $dirname
+cp system.in POSCAR $dirname
+sed -i "s/\(^ *potential_height_U0 =\).*/\1 $potential/" $dirname/system.in
+(
+    cd $dirname
+    /home/anooja/Work/tools/wannier_tools/utility/twisted_graphene_system_tight_binding/tgtbgen
+)
+cp wt.in $dirname/wt.in
+emin=-2
+emax=2
+(
+    cd $dirname
+    /home/anooja/Work/tools/wannier_tools/bin/wt.x
+    sed -i "s/\(^ *emin=\).*/\1 $emin/" bulkek.gnu
+    sed -i "s/\(^ *emax=\).*/\1 $emax/" bulkek.gnu
+    gnuplot bulkek.gnu
+)
--- Periodic_potential/tests/graphene_test/non_standard_cell_pbc/length_10nm/vary_potential.sh
+++ Periodic_potential/tests/graphene_test/non_standard_cell_pbc/length_10nm/vary_potential.sh
@@ -3,12 +3,13 @@
 # end=4
 # increment=1
 # for ((exponent = start; exponent <= end; exponent += increment)); do
-start=-1
-end=2
+start=1
+end=3
 increment=1
-for ((exponent = start; exponent <= end; exponent += increment)); do
-    potential=$(bc <<<"scale=1; 10^$exponent")
-    # potential=$(bc <<<"scale=1; $exponent")
+exponent=-1
+for ((value = start; value <= end; value += increment)); do
+    potential=$(bc <<<"scale=1; $value*10^$exponent")
+    echo $potential
     dirname=potential_$potential
     mkdir -p $dirname
     cp system.in POSCAR $dirname
@@ -18,24 +19,14 @@ for ((exponent = start; exponent <= end; exponent += increment)); do
         /home/anooja/Work/tools/wannier_tools/utility/twisted_graphene_system_tight_binding/tgtbgen
     )
     # TODO Automate the modification of wt.in to allow the unfolding process if required
-    cp wt.in-unfold $dirname/wt.in
-    emin=-2
-    emax=2
+    cp wt.in $dirname/wt.in
+    emin=0
+    emax=0.5
     (
         cd $dirname
         /home/anooja/Work/tools/wannier_tools/bin/wt.x
         sed -i "s/\(^ *emin=\).*/\1 $emin/" bulkek.gnu
         sed -i "s/\(^ *emax=\).*/\1 $emax/" bulkek.gnu
         gnuplot bulkek.gnu
-        gnuplot spectrum_unfold_kpath.gnu
     )
-    # emin=-2
-    # emax=2
-    # (
-    #     cd $dirname
-    #     /home/anooja/Work/tools/wannier_tools/bin/wt.x
-    #     sed -i "s/\(^ *emin=\).*/\1 $emin/" bulkek.gnu
-    #     sed -i "s/\(^ *emax=\).*/\1 $emax/" bulkek.gnu
-    #     gnuplot bulkek.gnu
-    # )
 done
