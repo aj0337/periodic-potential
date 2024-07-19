693c13117ed95621fb47a46eb393b512e680cabd
Anooja Jayaraj <anoojajayaraj@gmail.com>
2024-07-19 07:57:53 +0200

    Add important tests from wanniertools
    Remember that the TG_hr.dat is not saved

---
+++ bilayer_graphene/length_15nm/wt_with_TG/length_15nm/kronig/job_tb.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -l
+#SBATCH --no-requeue
+#SBATCH --job-name="defs1"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=8:00:00
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+#SBATCH --account="s1267"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=anooja.jayaraj@empa.ch
+#SBATCH --output=_scheduler-stdout.txt
+#SBATCH --error=_scheduler-stderr.txt
+
+module load daint-mc
+module load intel-oneapi/2021.3.0
+
+# E_L=0.005
+# Vext_values=(10 18 22 130 140 150 160)
+
+# for Vext in "${Vext_values[@]}"; do
+#     potential=$(echo "scale=2; $Vext * $E_L" | bc)
+#     dirname="potential_$potential"
+#     mkdir -p $dirname
+#     cp system.in POSCAR $dirname
+#     sed -i "s/\(^ *potential_height_U0 =\).*/\1 $potential/" $dirname/system.in
+#     (
+#         cd $dirname
+#         /users/ajayaraj/software/wannier_tools/bin/tgtbgen
+#     )
+# done
+
+start=0.25
+end=1.25
+increment=0.25
+value=$start
+while [ $(echo "$value <= $end" | bc) -eq 1 ]; do
+    potential=$(echo "scale=2; $value" | bc)
+    dirname=potential_$potential
+    mkdir -p $dirname
+    cp system.in POSCAR $dirname
+    sed -i "s/\(^ *potential_height_U0 =\).*/\1 $potential/" $dirname/system.in
+    (
+        cd $dirname
+        /users/ajayaraj/software/wannier_tools/bin/tgtbgen
+    )
+    value=$(echo "$value + $increment" | bc)
+done
---
+++ bilayer_graphene/length_15nm/wt_with_TG/length_15nm/triangular/sparse/potential_.02/job_tb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH --no-requeue
+#SBATCH --job-name="defs1"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+#SBATCH --account="s1267"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=anooja.jayaraj@empa.ch
+#SBATCH --output=_scheduler-stdout.txt
+#SBATCH --error=_scheduler-stderr.txt
+
+module load daint-mc
+module load intel-oneapi/2021.3.0
+
+
+/users/ajayaraj/software/wannier_tools/bin/tgtbgen
